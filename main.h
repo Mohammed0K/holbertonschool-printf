e1e61d7769435767881877154a4ed946551c00ae
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-25 06:03:37 +0000

    update

--- main.h
+++ main.h
@@ -15,7 +15,7 @@ int print_hex_pointer(uintptr_t n);
 int print_pointer(void *ptr);
 int process_format(const char *format, va_list args);
 int _printf(const char *format, ...);
-
+int handle_conversion(char specifier, va_list args);
 /* Prototypes for helper functions */
 int print_char(va_list args);
 int print_str(va_list args);
