becc85ca6002611f89196fe9233d155881a7528d
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-25 05:13:56 +0000

    update

--- main.h
+++ main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdint.h>
 /* Function Prototype */
+int print_hex_pointer(uintptr_t n);
 int _printf(const char *format, ...);
 int process_format(const char *format, va_list args);
 int handle_conversion(char specifier, va_list args);
 int print_number(int n);
 int print_unsigned_number(unsigned int n);
-#endif
+int print_octal(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
+int print_pointer(void *ptr);
 
+#endif
