062d16673e6bc72d4708b7c68a1e6e6ecd8b65b5
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-28 23:55:15 +0000

    try

--- print_pointer.c
+++ print_pointer.c
@@ -11,6 +11,15 @@ int print_pointer(void *ptr)
 	int count = 0;
 	uintptr_t address;
 
+	if (ptr == NULL)
+	{
+		count += buffered_putchar('(');
+		count += buffered_putchar('n');
+		count += buffered_putchar('i');
+		count += buffered_putchar('l');
+		count += buffered_putchar(')');
+		return (count);
+	}
 	count += buffered_putchar('0');
 	count += buffered_putchar('x');
 	address = (uintptr_t)ptr;
